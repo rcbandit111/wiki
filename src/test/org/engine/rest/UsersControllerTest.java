2e29cc525f0d87743021f281c63184cd34aa5316
plte1219 <plamen.terziev@netcracker.com>
2020-06-18 19:25:10 +0300

    Updated code

--- src/test/org/engine/rest/UsersControllerTest.java
+++ src/test/org/engine/rest/UsersControllerTest.java
@@ -1,19 +1,10 @@
 package org.engine.rest;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import net.bytebuddy.utility.RandomString;
 import org.engine.mapper.UserMapper;
 import org.engine.production.entity.Users;
 import org.engine.production.service.OldPasswordsService;
 import org.engine.production.service.UsersService;
-import org.engine.rest.UsersController;
 import org.engine.security.JwtTokenUtil;
 import org.engine.service.PasswordAdminResetHandler;
 import org.engine.service.UserRestService;
@@ -22,12 +13,19 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -49,60 +47,33 @@ public class UsersControllerTest {
             .parallel()
             .collect(Collectors.joining());
 
+    private final String ResetPasswordTokenDTO = Files.lines(Paths.get(ClassLoader.getSystemClassLoader().getResource("json/ResetPasswordTokenDTO.json").toURI()))
+            .parallel()
+            .collect(Collectors.joining());
+
     public UsersControllerTest() throws IOException, URISyntaxException {
     }
 
-
-//    private final String UserNewDTO = "{\n" +
-//            "   \"id\":\"1\",\n" +
-//            "   \"login\":\"login\",\n" +
-//            "   \"firstName\":\"firstName\",\n" +
-//            "   \"lastName\":\"lastName\",\n" +
-//            "   \"email\":\"email\",\n" +
-//            "   \"role\":\"role\",\n" +
-//            "   \"enabled\":\"true\",\n" +
-//            "   \"ownerId\":\"1\",\n" +
-//            "   \"ownerType\":\"ownerType\",\n" +
-//            "   \"lastActivityAt\":\"2020-05-14T17:45:55.9483536\",\n" +
-//            "   \"createdAt\":\"2019-07-14T17:45:55.9483536\"\n" +
-//            "}";
-
-//    private final String ResetUserDTO = "{\n" +
-//            "   \"name\":\"name\",\n" +
-//            "   \"email\":\"email\",\n" +
-//            "   \"status\":\"1\",\n" +
-//            "   \"error\":\"\",\n" +
-//            "   \"errorDescription\":\"\"\n" +
-//            "}";
-//
-//    private final String ResetPasswordTokenDTO = "{\n" +
-//            "   \"resetPasswordToken\":\"" + randomString_254 + "\",\n" +
-//            "   \"login\":\"login\",\n" +
-//            "   \"status\":\"1\",\n" +
-//            "   \"error\":\"\",\n" +
-//            "   \"errorDescription\":\"\"\n" +
-//            "}";
-
-    @Spy
+    @Mock
     public UsersService userService;
     private MockMvc mockMvc;
 
-    @Spy
+    @Mock
     public UserRestService userRestService;
 
     @InjectMocks
     private UsersController controller;
 
-    @Spy
+    @Mock
     private UserMapper userMapper;
 
-    @Spy
+    @Mock
     private PasswordEncoder passwordEncoder;
 
-    @Spy
+    @Mock
     private OldPasswordsService oldPasswordsService;
 
-    @Spy
+    @Mock
     private JwtTokenUtil jwtTokenUtil;
 
     @Mock
@@ -190,99 +161,70 @@ public class UsersControllerTest {
         MvcResult result = mockMvc.perform(post("/users/reset_request")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(ResetUserDTO))
-                .andExpect(status().isBadRequest())
+                .andExpect(status().isNotFound())
                 .andReturn();
 
-        assertEquals(result.getResponse().getContentAsString(), "NAME_AND_EMAIL_MISMATCH");
+        assertEquals(result.getResponse().getStatus(), 404);
     }
 
-//    @Test
-//    public void resetRequest_OK() throws Exception {
-//        when(userService.findByLogin(anyString())).thenReturn(trueOptional);
-//
-//        mockMvc.perform(post("/users/reset_request")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(ResetUserDTO))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.name").value("name"))
-//                .andExpect(jsonPath("$.email").value("email"))
-//                .andExpect(jsonPath("$.status").value("1"))
-//                .andExpect(jsonPath("$.error").value(""))
-//                .andExpect(jsonPath("$.errorDescription").value(""));
-//    }
-//
-//    @Test
-//    public void resetRequest_NOT_FOUND() throws Exception {
-//        when(userService.findByLogin(anyString())).thenReturn(Optional.empty());
-//
-//        mockMvc.perform(post("/users/reset_request")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(ResetUserDTO))
-//                .andExpect(status().isNotFound());
-//    }
-//
-//    @Test
-//    public void resetTokenTest_OK() throws Exception {
-//        when(userService.findByResetPasswordToken(anyString())).thenReturn(trueOptional);
-//
-//        mockMvc.perform(post("/users/reset_token")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(ResetPasswordTokenDTO))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.resetPasswordToken").value(randomString_254))
-//                .andExpect(jsonPath("$.login").value("login"))
-//                .andExpect(jsonPath("$.status").value("200"))
-//                .andExpect(jsonPath("$.error").value(""))
-//                .andExpect(jsonPath("$.errorDescription").value(""));
-//    }
-//
-//    @Test
-//    public void resetTokenTest_24_HOURS_PASSED() throws Exception {
-//        when(userService.findByResetPasswordToken(anyString())).thenReturn(falseTime);
-//
-//        mockMvc.perform(post("/users/reset_token")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(ResetPasswordTokenDTO))
-//                .andExpect(status().isNotFound());
-//    }
-//
-//    @Test
-//    public void resetTokenTest_NOT_FOUND() throws Exception {
-//        when(userService.findByResetPasswordToken(anyString())).thenReturn(Optional.empty());
-//
-//        mockMvc.perform(post("/users/reset_token")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(ResetPasswordTokenDTO))
-//                .andExpect(status().isNotFound());
-//    }
+    @Test
+    public void resetRequest_OK() throws Exception {
+        when(userService.findByLogin(anyString())).thenReturn(trueOptional);
 
+        mockMvc.perform(post("/users/reset_request")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ResetUserDTO))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.name").value("name"))
+                .andExpect(jsonPath("$.email").value("email"))
+                .andExpect(jsonPath("$.status").value("1"))
+                .andExpect(jsonPath("$.error").value(""))
+                .andExpect(jsonPath("$.errorDescription").value(""));
+    }
 
-   /*
     @Test
-    public void resetPasswordTest() throws Exception {
-    }
-    @Test
-    public void confirmationTokenTest() throws Exception {
-    }
-    @Test
-    public void resetUserPasswordTest() throws Exception {
-    }
-    @Test
-    public void resetConfirmationTest() throws Exception {
-    }
-    @Test
-    public void getTest() throws Exception {
-    }
-    @Test
-    public void saveTest() throws Exception {
-    }
-    @Test
-    public void getAllBySpecificationTest() throws Exception {
-    }*/
+    public void resetRequest_NOT_FOUND() throws Exception {
+        when(userService.findByLogin(anyString())).thenReturn(Optional.empty());
 
+        mockMvc.perform(post("/users/reset_request")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ResetUserDTO))
+                .andExpect(status().isNotFound());
+    }
 
+    @Test
+    public void resetTokenTest_OK() throws Exception {
+        when(userService.findByResetPasswordToken(anyString())).thenReturn(trueOptional);
 
-    /*@Test
-    public void pagesTest() throws Exception {
-    }*/
+        mockMvc.perform(post("/users/reset_token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ResetPasswordTokenDTO))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resetPasswordToken").value(randomString_254))
+                .andExpect(jsonPath("$.login").value("login"))
+                .andExpect(jsonPath("$.status").value("200"))
+                .andExpect(jsonPath("$.error").value(""))
+                .andExpect(jsonPath("$.errorDescription").value(""));
+    }
+
+    @Test
+    public void resetTokenTest_24_HOURS_PASSED() throws Exception {
+        when(userService.findByResetPasswordToken(anyString())).thenReturn(falseTime);
+
+        mockMvc.perform(post("/users/reset_token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ResetPasswordTokenDTO))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void resetTokenTest_NOT_FOUND() throws Exception {
+        when(userService.findByResetPasswordToken(anyString())).thenReturn(Optional.empty());
+
+        mockMvc.perform(post("/users/reset_token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ResetPasswordTokenDTO))
+                .andExpect(status().isNotFound());
+    }
+
 }
