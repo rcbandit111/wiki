da3ab21c98dd247d18e54492d09a5b6277646623
plte1219 <plamen.terziev@netcracker.com>
2020-06-23 03:33:17 +0300

    Updated code

--- src/main/java/org/engine/security/JwtTokenProvider.java
+++ src/main/java/org/engine/security/JwtTokenProvider.java
@@ -47,7 +47,9 @@ public class JwtTokenProvider {
     public String createToken(String username, List<Role> roles) {
 
         Claims claims = Jwts.claims().setSubject(username);
-        claims.put("auth", roles.stream().map(s -> new SimpleGrantedAuthority(s.getAuthority())).filter(Objects::nonNull).collect(Collectors.toList()));
+        claims.put("auth", roles.stream().filter(Objects::nonNull)
+                .map(s -> new SimpleGrantedAuthority(s.getAuthority()))
+                .collect(Collectors.toList()));
 
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
