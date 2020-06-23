f1d66656f722e2e87793b7a7ef5ce464c000c887
plte1219 <plamen.terziev@netcracker.com>
2020-06-23 19:44:13 +0300

    Updated code

--- src/main/java/org/engine/EngineApplication.java
+++ src/main/java/org/engine/EngineApplication.java
@@ -2,12 +2,9 @@ package org.engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @SpringBootApplication(scanBasePackages = { "org.engine.*", "org.engine.production.service", "org.engine.warehouse.service" })
 public class EngineApplication {
 
